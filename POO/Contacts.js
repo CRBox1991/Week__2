b010c36ffb53c7e6acd67a3c408934cc112ea395
CRBox <dharianfenix@gmail.com>
2024-02-27 16:14:13 +0100

    Segunda Subida

---
+++ POO/Contacts.js
@@ -0,0 +1,22 @@
+let {Person} = require("./Person")
+
+
+class Contacts{
+    constructor(){
+
+        this.person = [] 
+        this.name
+
+    }
+printPersons(){
+  
+    return this.person
+    }    
+}
+
+
+    
+    
+// console.log(contactos.printPersons());
+
+module.exports = {Contacts}
