9731f22d397171de94d7341be707d5f304d34b8a
CRBox <dharianfenix@gmail.com>
2024-03-02 16:13:14 +0100

    Subida de metodos distanceToOrigin y CalculateDistance

--- Algebra Project/points.ts
+++ Algebra Project/points.ts
@@ -33,37 +33,16 @@ public toString(x:number, y: number): string
 // como el punto inicial (0,0) se simplifica a distatanciaToOrigin = |x| + |y| se suma el valor absoluto
 public distanceToOrigin(): number
 {    
-    let distance: number = this.x + this.y
+// la distacia cartesiana es d = sqrt ((x2 - x1)**2 + (y2 - y1)**2) d = (x2 - 0)**2 + (y2 -0)**2
+// por lo que se simplifica a d = ((x**2) + (y**2))
+    let distance: number = Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y, 2))
     return distance
 }
-// formula para calcular la Distancia de Manhattan = |x2 - x1 | + |y2 - y1| 
-// en este caso anotherPoint es = |x - 0| + |y - 0| por lo que primero se calcula la distancia a 0
-//con distanToOrigin del punto 1, donde el resultado seria anotherPoint y se da como parametro de entrada para calculateDistance
-public calculateDistance(anotherPoint: number): number
+// la distacia cartesiana es d = sqrt ((x2 - x1)**2 + (y2 - y1)**2)
+public calculateDistance(anotherPoint: Point): number
 {   
-    let distance1: number = (this.x + this.y) - anotherPoint 
+    let distance1: number = Math.sqrt(((this.x - anotherPoint.getX())**2) + ((this.y - anotherPoint.getY())**2))
     return distance1
 }
-public getQuadrant(): number
-{   
-    let quadrantPosition: number = 0
-    if(this.x == 0 && this.y == 0)
-    {
-        return 0
-    }else if (this.x > 0 && this.y > 0)
-    {
-        return 1
-    } else if (this.x > 0 && this.y < 0)
-    {
-        return 2
-    } else if (this.x < 0 && this.y < 0)
-    {
-        return 3
-    } else if (this.x < 0 && this.y > 0)
-    {
-        return 4
-    }       
-    return quadrantPosition
-}   
-
 }
+
--- Algebra Project/pointsTest.ts
+++ Algebra Project/pointsTest.ts
@@ -1,6 +1,6 @@
 import { Point } from "./points";
 
-let point1 = new Point(0, 0)
+let point1 = new Point(45, 67)
 //console.log(point1.getX());
 //console.log(point1.getY());
 //console.log(point1.setX(12));
@@ -9,14 +9,17 @@ let point1 = new Point(0, 0)
 //console.log(point1.distanceToOrigin());
 
 
-let point2 = new Point(15,-45)
-let point3 = new Point(-14,-45)
-//console.log(point2.distanceToOrigin());
-//console.log(point2.calculateDistance(112));
-//console.log(point3.calculateDistance(112));
-console.log(point1.getQuadrant());
-console.log(point2.getQuadrant());
-console.log(point3.getQuadrant());
+let point2 = new Point(3,2)
+let point3 = new Point(1,4)
+console.log(point2.distanceToOrigin());
+console.log(point3.distanceToOrigin());
+console.log(point2.calculateDistance(point1));
+console.log(point3.calculateDistance(point1));
+//console.log(point1.getQuadrant());
+//console.log(point2.getQuadrant());
+//console.log(point3.getQuadrant());
+
+
 
 
 
