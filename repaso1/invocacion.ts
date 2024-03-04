ce2271167be75b51deec6d0ebc46f79f5f46c615
CRBox <dharianfenix@gmail.com>
2024-03-04 11:37:42 +0100

    invocacion

---
+++ repaso1/invocacion.ts
@@ -0,0 +1,9 @@
+import { add } from "./buclesFor";
+import { isEven } from "./condicionales";
+
+console.log(add(["Casa", "Coche", "Ciudad", "Cesta"]));
+isEven(20);
+console.log(add(["Barco", "Baca", "Bicicleta", "Balon", "Bisiesto", "Brasil"]));
+isEven(37);
+console.log(add(["Venezuela", "Veneno", "Voltaje"]));
+isEven(22);
