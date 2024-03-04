c185d0e72e4e4c375fd85763309d770b0050cdd0
CRBox <dharianfenix@gmail.com>
2024-03-04 11:18:41 +0100

    d

--- POO/Person.js
+++ POO/Person.js
@@ -38,18 +38,4 @@ Y mis hobbies son - ${this.hobbies}`
     }    
 }
 
-// let person1 = new Person(55,165)
-
-// person1.nombre = "Alicia";
-// person1.edad = 33;
-// person1.sexo = "Femenino";
-// person1.nacionalidad = "Argentina";
-// person1.dateOfBirth = 1991
-// person1.hobbies = ["Dormir", "Juagar play", "Leer"]
-
-// console.log(person1.calcularIMC(80,180));
-// console.log(person1.calcularEdad(2024,1991));
-// console.log(person1.printAll());
-// console.log(person1.printHobbies());
-
 module.exports = {Person}
